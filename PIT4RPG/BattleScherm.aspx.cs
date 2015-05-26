f498040c054ccbae9f4013ac03b06e08d7272337
mvdlaar <mvdlaar@gmail.com>
2015-05-26 09:28:10 +0200

    BattleScherm aangamaakt

---
+++ PIT4RPG/BattleScherm.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PIT4RPG.Model;
+
+namespace PIT4RPG
+{
+    public partial class BattleScherm : System.Web.UI.Page
+    {
+        private Character mijnCharacter;
+        private Monster mijnMonster;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["mijnCharacter"] != null)
+            {
+                mijnCharacter = (Character)Session["mijnCharacter"];
+            }
+            mijnMonster = new Monster();
+        }
+    }
+}
